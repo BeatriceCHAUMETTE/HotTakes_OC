a2dee52df7955461daaca5d1f1ea540ac18a2d2b
Beatrice Chaumette <beachaumette@gmail.com>
2022-08-17 10:27:40 +0200

    création du schéma de données sauce

---
+++ models/sauce.js
@@ -0,0 +1,20 @@
+//Import de Mongoose pour utilisation
+const mongoose = require('mongoose');
+
+//Création du schema de données
+const sauceSchema = mongoose.Schema({
+    /*userId: { type: String, required: true},*/
+    name: { type: String, required: true},
+    manufacturer: { type: String, required: true},
+    description: { type: String, required: true},
+    mainPepper: { type: String, required: true},
+    imageUrl: { type: String, required: true},
+    heat: { type: Number, required: true},
+    likes: { type: Number, required: true},
+    dislikes: { type: Number, required: true},
+    usersLiked: { type: ["String<userId>"], required: true},
+    usersDisliked: { type: ["String<userId>"], required: true},
+});
+
+//Export du modèle pour exploitation
+module.exports = mongoose.model('sauce', sauceSchema);
