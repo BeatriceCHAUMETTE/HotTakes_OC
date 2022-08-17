32d1dde138968b180eff9df1ffda74dcde1cf790
Beatrice Chaumette <beachaumette@gmail.com>
2022-08-17 12:25:22 +0200

    création du modele Utilisateur et des controllers et routes

---
+++ models/User.js
@@ -0,0 +1,18 @@
+//Import de Mongoose pour utilisation
+const mongoose = require('mongoose');
+
+//Ajout de unique validator pour éviter d'avoir plusieurs utilsateurs avec la même adresse mail
+const uniqueValidator = require('mongoose-unique-validator');
+
+//Création du schéma de données Utilisateur
+const userSchema = mongoose.Schema({
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true }
+});
+
+//Application du validateur au schéma 
+userSchema.plugin(uniqueValidator);
+
+//Export du modèle pour exploitation
+module.exports = mongoose.model('User', userSchema);
+
