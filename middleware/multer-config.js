e7ecfbdc2924eb5a281ea1232a55cafbfe5b7643
Beatrice Chaumette <beachaumette@gmail.com>
2022-08-17 22:16:13 +0200

    correction erreur syntaxe

--- middleware/multer-config.js
+++ middleware/multer-config.js
@@ -1,20 +1,24 @@
+// Import du package multer
 const multer = require('multer');
 
+// Préparation d'un dictionnaire formats images 
 const MIME_TYPES = {
   'image/jpg': 'jpg',
   'image/jpeg': 'jpg',
   'image/png': 'png'
 };
 
+// Création d'un objet de configuration pour multer
 const storage = multer.diskStorage({
-  destination: (req, file, callback) => {
-    callback(null, 'images');
+    destination: (req, file, callback) => {
+        callback(null, 'images');
   },
-  filename: (req, file, callback) => {
-    const name = file.originalname.split(' ').join('_');
-    const extension = MIME_TYPES[file.mimetype];
-    callback(null, name + Date.now() + '.' + extension);
+    filename: (req, file, callback) => {
+        const name = file.originalname.split(' ').join('_');
+        const extension = MIME_TYPES[file.mimetype];
+        callback(null, name + Date.now() + '.' + extension);
   }
 });
 
-module.exports = multer({storage: storage}).single('image');
+//Exportation du multer configuré
+module.exports = multer({ storage: storage }).single('image'); 
