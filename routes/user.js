3ea460347565ef49978f59ce76756975ab17319a
Beatrice Chaumette <beachaumette@gmail.com>
2022-08-23 23:04:41 +0200

    Amélioration des commentaires

--- routes/user.js
+++ routes/user.js
@@ -1,11 +1,11 @@
-// Création du router avec la méthode router d'express
+//Création du router avec la méthode router d'express
 const express = require('express');
 const router = express.Router();
 
-// Configuration du router
+//Import du controller user
 const userCtrl = require('../controllers/user');
 
-//Création des routes
+//Enregistrement des routes de connexion et de création de compte
 router.post('/signup', userCtrl.signup);
 router.post('/login', userCtrl.login);
 
