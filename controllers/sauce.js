3f9ce30ff3809bb4dd73085fe79d41ca798eada1
BeaChaumette <beachaumette@gmail.com>
2022-09-09 10:53:30 +0200

    Modification controller sauce pour supprimer image stockée quand modification sauce

--- controllers/sauce.js
+++ controllers/sauce.js
@@ -93,12 +93,21 @@ exports.modifySauce = (req, res) => {
         if (sauce.userId != req.auth.userId) {
             res.status(401).json({ message: 'Non autorisé !'});
         } else {
-            Sauce.updateOne({ _id: req.params.id }, { ...sauceObject, _id: req.params.id })
-            .then(() => res.status(200).json({ message: 'Sauce modifiée !' }))
-            .catch(error => res.status(401).json({ error }));
+            if(req.file) {
+                const filename = sauce.imageUrl.split('/images/')[1];
+                fs.unlink(`images/${filename}`, () => {
+                    Sauce.updateOne({ _id: req.params.id }, { ...sauceObject, _id: req.params.id })
+                    .then(() => res.status(200).json({ message: 'Sauce modifiée !' }))
+                    .catch(error => res.status(401).json({ error }));
+                });           
+            } else { 
+                Sauce.updateOne({ _id: req.params.id }, { ...sauceObject, _id: req.params.id })
+                .then(() => res.status(200).json({ message: 'Sauce modifiée !' }))
+                .catch(error => res.status(401).json({ error }));
+            }           
         }
     })
-    .catch(error => res.status(400).json({ error }));
+    .catch(error => res.status(400).json({ error }));   
 };    
     
 //Suppression d'une sauce
